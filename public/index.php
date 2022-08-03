dde95b39a7c0dbde276f26407f546fdc030fd701
John Doe <y.mazanik@manao-team.com>
2022-08-03 15:04:45 +0300

    x frame fix

--- public/index.php
+++ public/index.php
@@ -1,7 +1,5 @@
 <?php
-	header('X-Frame-Op4tions: SAMESITE');
-	header('X-Frame-Op4tions: 1');
-	header('X-Frame-Op4tions: 2');
+
 use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
